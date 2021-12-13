788713b600f3620b4feb89b8889bce6c31cac876
Вячеслав Гуськов <v.o.gusjkov@student.khai.edu>
2021-12-13 18:10:47 +0200

    TicTacToe 2 Stage

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,2 +1,12 @@
 # Этап 1. Сетка.
 print("X O X\n""O X O\n""X X O")
+# Этап 2. Игровое поле
+def tictactoeboard():
+    print('Enter cells:')
+    tttb = list(input())
+    print('---------')
+    print('| ' + tttb[0] + ' ' + tttb[1] + ' ' + tttb[2] + ' |')
+    print('| ' + tttb[3] + ' ' + tttb[4] + ' ' + tttb[5] + ' |')
+    print('| ' + tttb[6] + ' ' + tttb[7] + ' ' + tttb[8] + ' |')
+    print('---------')
+tictactoeboard() 
