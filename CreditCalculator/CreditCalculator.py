6aa8f6846bb31db2aed32a651731086370639a72
Вячеслав Гуськов <v.o.gusjkov@student.khai.edu>
2021-12-19 21:04:12 +0200

    CreditCalculator 3 Stage

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -3,22 +3,42 @@ import math
 # Этап 1. Текстовые сообщения калькулятора.
 print("Loan principal: 1000\nMonth 1: repaid 250\nMonth 2: repaid 250\nMonth 3: repaid 500\nThe loan has been repaid!")
 
-# Этап 2. Расчёт кредита
-print("Enter the loan principal:")
-total_pay = float(input("> "))
-print("""What do you want to calculate?\ntype "m" – for number of monthly payments,\ntype "p" – for the monthly payment:""")
+# Этап 2-3. Расчёт кредита
+print("""What do you want to calculate?
+type "n" for number of monthly payments,
+type "a" for annuity monthly payment amount,
+type "p" for loan principal:""")
 type_inp = input("> ")
-if type_inp == "m":
+if type_inp == "n":
+    print("Enter the loan principal:")
+    loan_principal = float(input("> "))
     print("Enter the monthly payment:")
-    one_months = int(input("> "))
-    number_months = math.ceil(total_pay / one_months)
-    print("It will take " + str(number_months) + " months to repay the loan")
+    monthly_payment = float(input("> "))
+    print("Enter the loan interest:")
+    loan_interest = float(input("> "))
+    mounths_count = math.ceil(math.log(monthly_payment / (monthly_payment - (loan_interest / 1200 * loan_principal)), (1 + loan_interest / 1200)))
+    if mounths_count // 12 == 0:
+        info_date = str(mounths_count) + " months"
+    if mounths_count % 12 == 0:
+        info_date = str(mounths_count // 12) + " years"
+    if mounths_count % 12 != 0:
+        info_date = str(mounths_count // 12) + " years and " + str(mounths_count % 12) + " months"
+    print("It will take " + info_date + " to repay this loan!")
+if type_inp == "a":
+    print("Enter the loan principal:")
+    loan_principal = float(input("> "))
+    print("Enter the number of periods:")
+    number_periods = float(input("> "))
+    print("Enter the loan interest:")
+    loan_interest = float(input("> "))
+    monthly_payment = math.ceil(loan_principal * ((loan_interest / 1200) * ((1 + loan_interest / 1200) ** number_periods) / (((1 + loan_interest / 1200) ** number_periods) - 1)))
+    print("Your monthly payment = " + str(monthly_payment) + "!")
 if type_inp == "p":
-    print("Enter the number of months:")
-    number_months = int(input("> "))
-    one_months = math.ceil(total_pay / number_months)
-    last_pay = total_pay - (number_months - 1) * one_months
-    if last_pay == one_months:
-        print("Your monthly payment = " + str(one_months))
-    else:
-        print("Your monthly payment = " + str(one_months) + " and the last payment = " + str(last_pay) + ".")
+    print("Enter the annuity payment:")
+    annuity_payment = float(input("> "))
+    print("Enter the number of periods:")
+    number_periods = float(input("> "))
+    print("Enter the loan interest:")
+    loan_interest = float(input("> "))
+    loan_principal = round(annuity_payment / ((loan_interest / 1200) * ((1 + loan_interest / 1200) ** number_periods) / (((1 + loan_interest / 1200) ** number_periods) - 1)))
+    print("Your loan principal = " + str(loan_principal) + "!")
