1e37362fc7a5e0bf05a42041cc4d8976de2b9f9c
Вячеслав Гуськов <v.o.gusjkov@student.khai.edu>
2021-12-19 22:35:40 +0200

    CreditCalculator 4 Stage

--- CreditCalculator/CreditCalculator.py
+++ CreditCalculator/CreditCalculator.py
@@ -1,21 +1,8 @@
+# Этап 4. Расчёт кредита по заданным параметрам через аргументы командной строки.
 import math
+import argparse
 
-# Этап 1. Текстовые сообщения калькулятора.
-print("Loan principal: 1000\nMonth 1: repaid 250\nMonth 2: repaid 250\nMonth 3: repaid 500\nThe loan has been repaid!")
-
-# Этап 2-3. Расчёт кредита
-print("""What do you want to calculate?
-type "n" for number of monthly payments,
-type "a" for annuity monthly payment amount,
-type "p" for loan principal:""")
-type_inp = input("> ")
-if type_inp == "n":
-    print("Enter the loan principal:")
-    loan_principal = float(input("> "))
-    print("Enter the monthly payment:")
-    monthly_payment = float(input("> "))
-    print("Enter the loan interest:")
-    loan_interest = float(input("> "))
+def annuity_periods_C(loan_principal, monthly_payment, loan_interest):
     mounths_count = math.ceil(math.log(monthly_payment / (monthly_payment - (loan_interest / 1200 * loan_principal)), (1 + loan_interest / 1200)))
     if mounths_count // 12 == 0:
         info_date = str(mounths_count) + " months"
@@ -24,21 +11,54 @@ if type_inp == "n":
     if mounths_count % 12 != 0:
         info_date = str(mounths_count // 12) + " years and " + str(mounths_count % 12) + " months"
     print("It will take " + info_date + " to repay this loan!")
-if type_inp == "a":
-    print("Enter the loan principal:")
-    loan_principal = float(input("> "))
-    print("Enter the number of periods:")
-    number_periods = float(input("> "))
-    print("Enter the loan interest:")
-    loan_interest = float(input("> "))
+    print("Overpayment = " + str(int(monthly_payment * mounths_count - loan_principal)))
+
+def annuity_principal_C(monthly_payment, number_periods, loan_interest):
+    loan_principal = round(monthly_payment / ((loan_interest / 1200) * ((1 + loan_interest / 1200) ** number_periods) / (((1 + loan_interest / 1200) ** number_periods) - 1)))
+    print("Your loan principal = " + str(loan_principal) + "!")
+    print("Overpayment = " + str(int(monthly_payment * number_periods - loan_principal)))
+
+def annuity_payment_C(loan_principal, number_periods, loan_interest):
     monthly_payment = math.ceil(loan_principal * ((loan_interest / 1200) * ((1 + loan_interest / 1200) ** number_periods) / (((1 + loan_interest / 1200) ** number_periods) - 1)))
     print("Your monthly payment = " + str(monthly_payment) + "!")
-if type_inp == "p":
-    print("Enter the annuity payment:")
-    annuity_payment = float(input("> "))
-    print("Enter the number of periods:")
-    number_periods = float(input("> "))
-    print("Enter the loan interest:")
-    loan_interest = float(input("> "))
-    loan_principal = round(annuity_payment / ((loan_interest / 1200) * ((1 + loan_interest / 1200) ** number_periods) / (((1 + loan_interest / 1200) ** number_periods) - 1)))
-    print("Your loan principal = " + str(loan_principal) + "!")
+    print("Overpayment = " + str(int(monthly_payment * number_periods - loan_principal)))
+
+def annuity_C(interest, principal, periods, payment):
+    if periods == None:
+        annuity_periods_C(principal, payment, interest)
+    if principal == None:
+        annuity_principal_C(payment, periods , interest)
+    if payment == None:
+        annuity_payment_C(principal, periods, interest)
+
+def diff_C(loan_interest, loan_principal, number_periods):
+    main_duty = 0
+    for mounth_num in range(1, number_periods + 1):
+        differ_payment = math.ceil(float(loan_principal / number_periods) + (loan_interest / 1200) * (loan_principal - loan_principal * (mounth_num - 1) / number_periods))
+        print('Month ' + str(mounth_num) + ': payment is ' + str(differ_payment))
+        main_duty += differ_payment
+    print('Overpayment = ' + str(int(main_duty - loan_principal)))
+
+parser = argparse.ArgumentParser(description='CreditCalc')
+parser.add_argument('--type', type=str)
+parser.add_argument('--payment', type=float)
+parser.add_argument('--principal', type=float)
+parser.add_argument('--periods', type=int)
+parser.add_argument('--interest', type=float)
+args = parser.parse_args()
+
+if args.type == "annuity":
+    if args.interest != None:
+        if (args.principal != None and args.periods != None) or (args.principal != None and args.payment != None) or (args.periods != None and args.payment != None):
+            annuity_C(args.interest, args.principal, args.periods, args.payment)
+        else:
+            print("Incorrect parameters")
+    else:
+        print("Incorrect parameters")
+elif args.type == "diff":
+    if args.payment == None and args.interest != None and args.principal != None and args.periods != None:
+        diff_C(args.interest, args.principal, args.periods)
+    else:
+        print("Incorrect parameters")
+else:
+    print("Incorrect parameters")
