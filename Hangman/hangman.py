70515b944c61d010caaa0c233456190bfa37794c
Вячеслав Гуськов <v.o.gusjkov@student.khai.edu>
2021-12-26 17:02:42 +0200

    Hangman. Rework. Stage 5

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,9 +3,20 @@ import random
 print("HANGMAN")
 list_word = ["python", "java", "javascript", "php"]
 win_word = random.choice(list_word)
-text = "Guess the word "+win_word[0:3]+("-"*(len(win_word)-3))+": > "
-word = input(text)
-if word == win_word:
-    print("You survived!")
-else:
-    print("You lost!")
+word_progress = []
+for ii in range(len(win_word)):
+    word_progress.append("-")
+word_step = 8
+while True:
+    print("".join(word_progress))
+    symbola = input("Input a letter: > ")
+    if symbola not in win_word:
+        print("That letter doesn't appear in the word")
+        continue
+    for ii in range(0, len(win_word)):
+        if symbola == win_word[ii]:
+            word_progress[ii] = symbola
+    word_step -= 1
+    if word_step == 0:
+        print("Thanks for playing!\nWe'll see how well you did in the next stage")
+        break
