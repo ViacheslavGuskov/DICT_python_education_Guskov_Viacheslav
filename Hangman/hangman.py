10c098165d119a95f99581c77434b7b8e396bf92
Вячеслав Гуськов <v.o.gusjkov@student.khai.edu>
2021-12-26 17:05:04 +0200

    Hangman. Rework. Stage 7

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,6 +3,8 @@ import random
 print("HANGMAN")
 list_word = ["python", "java", "javascript", "php"]
 win_word = random.choice(list_word)
+success_symbol = list("qwertyuiopasdfghjklzxcvbnm")
+input_symbol = []
 word_progress = []
 for ii in range(len(win_word)):
     word_progress.append("-")
@@ -16,13 +18,19 @@ while True:
         print("You lost!")
         break
     symbola = input("Input a letter: > ")
+    if len(symbola) != 1:
+        print("You should input a single letter")
+        continue
+    if symbola not in success_symbol:
+        print("Please enter a lowercase English letter")
+        continue
+    if symbola in input_symbol:
+        print("You've already guessed this letter")
+        continue
+    input_symbol.append(symbola)
     if symbola not in win_word:
         word_step -= 1
         print("That letter doesn't appear in the word")
-        continue
-    if symbola in word_progress:
-        word_step -= 1
-        print("No improvements")
         continue
     for ii in range(0, len(win_word)):
         if symbola == win_word[ii]:
