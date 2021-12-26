22e98e0141769cdbd667f2c52171d9ea39dfd3d6
Вячеслав Гуськов <v.o.gusjkov@student.khai.edu>
2021-12-26 17:01:18 +0200

    Hangman. Rework. Stage 4

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,7 +3,8 @@ import random
 print("HANGMAN")
 list_word = ["python", "java", "javascript", "php"]
 win_word = random.choice(list_word)
-word = input("Guess the word: > ")
+text = "Guess the word "+win_word[0:3]+("-"*(len(win_word)-3))+": > "
+word = input(text)
 if word == win_word:
     print("You survived!")
 else:
