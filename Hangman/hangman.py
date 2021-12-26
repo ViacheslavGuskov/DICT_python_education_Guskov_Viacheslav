b5b78a0f650a5ceb991bc2f627b9fcd3a367f5b8
Вячеслав Гуськов <v.o.gusjkov@student.khai.edu>
2021-12-26 17:03:23 +0200

    Hangman. Rework. Stage 6

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -9,14 +9,21 @@ for ii in range(len(win_word)):
 word_step = 8
 while True:
     print("".join(word_progress))
+    if "-" not in word_progress:
+        print("You guessed the word!\nYou survived!")
+        break
+    if word_step == 0:
+        print("You lost!")
+        break
     symbola = input("Input a letter: > ")
     if symbola not in win_word:
+        word_step -= 1
         print("That letter doesn't appear in the word")
+        continue
+    if symbola in word_progress:
+        word_step -= 1
+        print("No improvements")
         continue
     for ii in range(0, len(win_word)):
         if symbola == win_word[ii]:
             word_progress[ii] = symbola
-    word_step -= 1
-    if word_step == 0:
-        print("Thanks for playing!\nWe'll see how well you did in the next stage")
-        break
